e1c6c552bdac9e19e1618a4c554893c97f3b44ea
tishkinaa <tishkinalesha@gmail.com>
2020-06-09 15:35:04 +0300

    SnakeEating2

---
+++ Snake2020/FoodCreator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake2020
+{
+    class FoodCreator
+    {
+        int maxWidth;
+        int maxHeight;
+        char sym;
+
+        Random random = new Random();
+        public FoodCreator(int maxWidth, int maxHeight, char sym)
+        {
+            this.maxWidth = maxWidth;
+            this.maxHeight = maxHeight;
+            this.sym = sym;
+        }
+
+        public Point CreateFood()
+        {
+            int x = random.Next(2, maxWidth - 2);
+            int y = random.Next(2, maxHeight - 2);
+            return new Point(x, y, sym);
+        }
+    }
+}
